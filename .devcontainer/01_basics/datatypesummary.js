145c0627a9c12340aee69698354005a6473a2a91
sagar muktan <124330466+sagarmoktan20@users.noreply.github.com>
2024-04-27 16:38:34 +0000

    datatypes summary

---
+++ .devcontainer/01_basics/datatypesummary.js
@@ -0,0 +1,40 @@
+//primitive
+//7 types = string,number,boolean,null,undefined,symbol,bigInt
+
+//Refrence type(non primitive)
+//array,objects,functions
+
+//symbol usage
+const id=Symbol("123")
+const id2=Symbol("123")
+console.log(id==id2)
+
+//array declaration
+const sagar =[developer,android ,software,kotlin,js,node_js,]
+
+//object declaration
+let myobj={
+    name:"sagar",
+    age:21
+}
+
+//here we can assign a function to a variable too
+const myFunc= function(){
+    console.log("hello world!")
+}
+
+//datatypes they return are
+
+/*
+string->string
+boolen->boolean
+number->number
+null->object
+undefined->undefined
+symbol->symbol
+big int->bingint
+
+array-> array
+object->object
+function->function
+*/
