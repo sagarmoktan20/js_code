460fb072d32ca8b4c3f05540647198ad2f3abca2
sagar muktan <124330466+sagarmoktan20@users.noreply.github.com>
2024-04-27 12:35:39 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId =1234
+let accountEmail ="sagar@gmail.com"//prefer let over var cause it solved the problem of scope
+var accountPassword="12345"//prefer not to use var cause it has scope problems
+accoutnCity="jaipur"
+let account_state;
+
+// accountId=10 
+
+accountEmail="sunny@gamail.com"
+accountPassword="sahar"
+accoutnCity="banglore"
+
+
+console.table([accountEmail,accountId,accountPassword,accoutnCity,account_state])
+
+console.log(accountId)
